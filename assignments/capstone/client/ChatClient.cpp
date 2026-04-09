4e6b2b8b14adec2dbc2da310a1e9312cb5beb696
deepsprof <pradeep.selvakumar@bhcc.edu>
2026-04-09 08:31:05 -0400

    Added error handling

--- assignments/capstone/client/ChatClient.cpp
+++ assignments/capstone/client/ChatClient.cpp
@@ -8,6 +8,7 @@
 #pragma comment(lib, "Ws2_32.lib")
 #else
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #define closesocket close
@@ -17,19 +18,46 @@ int main()
 {
 #ifdef _WIN32
     WSADATA data;
-    WSAStartup(MAKEWORD(2, 2), &data);
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+    {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
 #endif
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        std::cerr << "Failed to create socket\n";
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
     sockaddr_in serverHint{};
     serverHint.sin_family = AF_INET;
     serverHint.sin_port = htons(54000);
-    inet_pton(AF_INET, "127.0.0.1", &serverHint.sin_addr);
+
+    int ptonResult = inet_pton(AF_INET, "127.0.0.1", &serverHint.sin_addr);
+    if (ptonResult <= 0)
+    {
+        std::cerr << "Invalid address or address conversion failed\n";
+        closesocket(sock);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
 
     int conn = connect(sock, (sockaddr *)&serverHint, sizeof(serverHint));
     if (conn < 0)
     {
-        std::cerr << "Cannot connect";
+        std::cerr << "Cannot connect to server\n";
+        closesocket(sock);
+#ifdef _WIN32
+        WSACleanup();
+#endif
         return 1;
     }
 
@@ -38,16 +66,40 @@ int main()
 
     while (true)
     {
-        std::getline(std::cin, input);
+        std::cout << "> ";
+        if (!std::getline(std::cin, input))
+        {
+            std::cerr << "Input error\n";
+            break;
+        }
+
         if (input == "quit")
             break;
 
-        send(sock, input.c_str(), input.size(), 0);
+        int bytesSent = send(sock, input.c_str(), static_cast<int>(input.size()), 0);
+        if (bytesSent < 0)
+        {
+            std::cerr << "Failed to send data to server\n";
+            break;
+        }
 
-        memset(buffer, 0, 4096);
-        int bytesReceived = recv(sock, buffer, 4096, 0);
+        std::memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+
         if (bytesReceived > 0)
-            std::cout << "Server: " << std::string(buffer, bytesReceived) << std::endl;
+        {
+            std::cout << "Server: " << std::string(buffer, bytesReceived) << '\n';
+        }
+        else if (bytesReceived == 0)
+        {
+            std::cout << "Server disconnected\n";
+            break;
+        }
+        else
+        {
+            std::cerr << "Receive failed\n";
+            break;
+        }
     }
 
     closesocket(sock);
--- assignments/capstone/server/ChatServer.cpp
+++ assignments/capstone/server/ChatServer.cpp
@@ -8,6 +8,7 @@
 #pragma comment(lib, "Ws2_32.lib")
 #else
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #define closesocket close
@@ -17,13 +18,20 @@ int main()
 {
 #ifdef _WIN32
     WSADATA data;
-    WSAStartup(MAKEWORD(2, 2), &data);
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+    {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
 #endif
 
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening < 0)
     {
-        std::cerr << "Cannot create socket";
+        std::cerr << "Cannot create socket\n";
+#ifdef _WIN32
+        WSACleanup();
+#endif
         return 1;
     }
 
@@ -32,27 +40,80 @@ int main()
     hint.sin_port = htons(54000);
     hint.sin_addr.s_addr = INADDR_ANY;
 
-    bind(listening, (sockaddr *)&hint, sizeof(hint));
-    listen(listening, SOMAXCONN);
+    if (bind(listening, (sockaddr *)&hint, sizeof(hint)) < 0)
+    {
+        std::cerr << "Bind failed\n";
+        closesocket(listening);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    if (listen(listening, SOMAXCONN) < 0)
+    {
+        std::cerr << "Listen failed\n";
+        closesocket(listening);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
 
     sockaddr_in client{};
     socklen_t clientSize = sizeof(client);
 
     int clientSocket = accept(listening, (sockaddr *)&client, &clientSize);
+    if (clientSocket < 0)
+    {
+        std::cerr << "Accept failed\n";
+        closesocket(listening);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
     closesocket(listening);
+
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    std::memset(host, 0, sizeof(host));
+    std::memset(service, 0, sizeof(service));
+
+    inet_ntop(AF_INET, &client.sin_addr, host, sizeof(host));
+    std::cout << "Client connected from " << host
+              << ":" << ntohs(client.sin_port) << '\n';
 
     char buffer[4096];
     while (true)
     {
-        memset(buffer, 0, 4096);
-        int bytesReceived = recv(clientSocket, buffer, 4096, 0);
-        if (bytesReceived <= 0)
+        std::memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+        if (bytesReceived > 0)
+        {
+            std::string msg(buffer, bytesReceived);
+            std::cout << "Client: " << msg << '\n';
+
+            int bytesSent = send(clientSocket, buffer, bytesReceived, 0);
+            if (bytesSent < 0)
+            {
+                std::cerr << "Send failed\n";
+                break;
+            }
+        }
+        else if (bytesReceived == 0)
+        {
+            std::cout << "Client disconnected\n";
             break;
-
-        std::string msg(buffer, bytesReceived);
-        std::cout << "Client: " << msg << std::endl;
-
-        send(clientSocket, buffer, bytesReceived, 0);
+        }
+        else
+        {
+            std::cerr << "Receive failed\n";
+            break;
+        }
     }
 
     closesocket(clientSocket);
